894b6b25d02dfe9e6c735b22aaeb320adac60c2c
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-06-13 17:49:31 -0500

    install next-auth, prisma client, prisma adapter, bcrypt

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,8 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    // require('@tailwindcss/forms')({
-    //   strategy: 'class',
-    // }),
+    require('@tailwindcss/forms')({
+      strategy: 'class',
+    }),
   ],
 };
