396ba9971ffe8acbd2033344aa1e9e097a3af356
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-06-30 18:47:38 -0500

    add footer navigation for mobile view

--- app/components/sidebar/SideBar.tsx
+++ app/components/sidebar/SideBar.tsx
@@ -1,4 +1,5 @@
 import DesktopSidebar from './DesktopSidebar';
+import MobileFooter from './MobileFooter';
 
 interface SideBarProps {
   children: React.ReactNode;
@@ -8,6 +9,7 @@ export default async function SideBar({ children }: SideBarProps) {
   return (
     <div className='h-full'>
       <DesktopSidebar />
+      <MobileFooter />
       <main className='lg:pl-20 h-full'>{children}</main>
     </div>
   );
