37ffa983a90137cab09e12dd84a4ff1a7fd7e319
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-06-30 20:02:58 -0500

    sucessfully fetch user data from db + next-superjson-plugin

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -9,9 +9,12 @@ interface DesktopSidebarProps {
   currentUser: User;
 }
 
-export default function DesktopSidebar({}: DesktopSidebarProps) {
+export default function DesktopSidebar({ currentUser }: DesktopSidebarProps) {
   const routes = useRoutes();
   const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  console.log(currentUser);
+
   return (
     <div
       className='hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between
