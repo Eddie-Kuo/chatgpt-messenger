825b792c35aff704b73da3650329e30590ab8347
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-07-02 16:37:22 -0500

    add placeholder for online status

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -14,8 +14,9 @@ export default function Avatar({ user }: AvatarProps) {
           alt='Avatar'
           src={user?.image || '/images/placeholder.jpg'}
           fill
-        ></Image>
+        />
       </div>
+      <span className='absolute block rounded-full bg-green-500 ring-2 ring-white top-0 right-0 h-2 w-2 md:h-3 md:w-3' />
     </div>
   );
 }
