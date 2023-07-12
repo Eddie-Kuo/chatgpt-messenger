f7da3f36c09969f9aeee37e992cd4ebed1cdc904
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-07-12 16:14:27 -0500

    add settings modal ui

--- app/components/navSidebar/SettingsModal.tsx
+++ app/components/navSidebar/SettingsModal.tsx
@@ -1,9 +1,14 @@
 'use client';
 
 import { User } from '@prisma/client';
+import { CldUploadButton } from 'next-cloudinary';
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
-import { FieldValues, useForm } from 'react-hook-form';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import Modal from '../Modal';
+import Button from '../ui/Button';
+import Input from '../ui/Input';
 
 interface SettingsModalProps {
   currentUser: User;
@@ -19,12 +24,94 @@ export default function SettingsModal({
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
 
-  const {} = useForm<FieldValues>({
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    formState: { errors },
+  } = useForm<FieldValues>({
     defaultValues: {
       name: currentUser?.name,
       image: currentUser?.image,
     },
   });
 
-  return <div>SettingsModal</div>;
+  const image = watch('image');
+
+  const handleUpload = (result: any) => {
+    setValue('image', result?.info?.secure_url, {
+      shouldValidate: true,
+    });
+  };
+
+  const onSubmit: SubmitHandler<FieldValues> = () => {
+    setIsLoading(true);
+
+    // axios call
+    // set is loading back to false
+  };
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className='space-y-12'>
+          <div className='border-b border-blue-500 pb-10'>
+            <h2 className='text-base font-semibold leading-7 text-gray-900'>
+              Profile Settings
+            </h2>
+            <p className='mt-1 text-sm leading-6 text-gray-500 '>
+              Edit your profile information below. Remember to click save for
+              the changes to take effect!
+            </p>
+
+            <div className='mt-6 flex flex-col gap-y-8'>
+              <Input
+                disabled={isLoading}
+                label='Name'
+                id='name'
+                errors={errors}
+                required
+                register={register}
+              />
+              <div>
+                <label className='block text-sm font-medium leading-6 text-gray-900'>
+                  Photo
+                </label>
+                <div className='mt-2 flex items-center gap-x-3'>
+                  <Image
+                    width='48'
+                    height='48'
+                    className='rounded-full'
+                    src={
+                      image || currentUser?.image || '/images/placeholder.jpg'
+                    }
+                    alt='avatar'
+                  />
+                  <CldUploadButton
+                    options={{ maxFiles: 1 }}
+                    onUpload={handleUpload}
+                    uploadPreset='czoj7dlw'
+                  >
+                    <Button disabled={isLoading} secondary type='button'>
+                      Change
+                    </Button>
+                  </CldUploadButton>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='mt-6 flex items-center justify-end gap-x-6'>
+          <Button disabled={isLoading} secondary onClick={onClose}>
+            Cancel
+          </Button>
+          <Button disabled={isLoading} type='submit'>
+            Save
+          </Button>
+        </div>
+      </form>
+    </Modal>
+  );
 }
