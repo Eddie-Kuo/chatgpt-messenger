e0c60072d742e1d6988c793c89662ccd9a97ae8c
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-07-14 12:58:11 -0500

    type declarations - select component

--- app/components/ui/Select.tsx
+++ app/components/ui/Select.tsx
@@ -1,7 +1,19 @@
 ' use client';
 
-interface SelectProps {}
+interface SelectProps {
+  label: string;
+  value?: Record<string, any>;
+  onChange: (value: Record<string, any>) => void;
+  options: Record<string, any>[];
+  disabled?: boolean;
+}
 
-export default function Select({}: SelectProps) {
+export default function Select({
+  label,
+  value,
+  onChange,
+  options,
+  disabled,
+}: SelectProps) {
   return <div>Select</div>;
 }
