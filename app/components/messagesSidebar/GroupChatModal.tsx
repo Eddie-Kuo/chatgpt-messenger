4512345d8c9a84a1e07c0ea754c01083d28f6d9f
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-07-14 12:27:25 -0500

    initialize group chat modal

---
+++ app/components/messagesSidebar/GroupChatModal.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+interface GroupChatModalProps {
+  isOpen?: boolean;
+  onClose: () => void;
+}
+
+export default function GroupChatModal({
+  isOpen,
+  onClose,
+}: GroupChatModalProps) {
+  return <div>GroupChatModal</div>;
+}
