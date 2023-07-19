9b8dcf16a29af8261d4e8e5e9f51f9793ee5e47a
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-07-19 14:34:30 -0500

    change to ts ignore

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -10,7 +10,7 @@ export default async function DashboardLayout({
   const users = await getUsers();
 
   return (
-    // @ts-expect-error Server Component
+    // @ts-ignore
     <SideBar>
       <div className='h-full'>
         <UserList items={users} />
