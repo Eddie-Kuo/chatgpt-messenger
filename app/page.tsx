9844936aa05486d3e0c033a0d2fdb912080a97aa
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-07-19 14:38:03 -0500

    remove blank landing page

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-interface pageProps {}
-
-export default function page({}: pageProps) {
-  return (
-    <div className='flex h-screen items-center justify-center'>
-      <p>Hello World!</p>
-    </div>
-  );
-}
